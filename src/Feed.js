14eb1f17fe92230e55d927d230f139769024bb8a
annesard <a.ryabkova@innopolis.university>
2021-09-15 19:47:34 +0300

    finish feed

--- src/Feed.js
+++ src/Feed.js
@@ -8,11 +8,28 @@ function Feed() {
             <div className="post">
                 <div className="post-user-name-time">
                     <ul>
-                        <li className="post-user"> </li>
-                        <li className="post-username">User Darryl</li>
+                        <li className="post-user li-feed"> </li>
+                        <li className="li-feed">User Darryl</li>
 
                     </ul>
+                    <div className="description-pic">
+                        <div className="description-feed">
+                            Check out this exquisite painting from Luxembourgh!...
+                        </div>
+                        <div className="pic-feed">
+
+                        </div>
+                    </div>
+                    <div >
+                        <ul className="footer-desc-pic">
+                            <li className="like-pic"></li>
+                            <li className="like-feed">Like</li>
+                            <li className="comment-pic"></li>
+                            <li className="comment-feed">Comment </li>
+                        </ul>
+                    </div>
                 </div>
+
 
             </div>
         </div>
