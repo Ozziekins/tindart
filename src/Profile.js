7829ce58d006dfa53ff8f9a92ccd07dd2300df69
annesard <a.ryabkova@innopolis.university>
2021-09-15 20:53:20 +0300

    foundation for profile

---
+++ src/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./profile.css"
+function Profile() {
+    return (
+        <div>
+            <div className="logo-feed"/>
+            <div className="user"/>
+
+
+        </div>
+    );
+}
+
+export default Profile;
