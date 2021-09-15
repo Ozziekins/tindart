5d221e16dcd0b667753d9ba29b773f2e12ff99a2
annesard <a.ryabkova@innopolis.university>
2021-09-15 22:23:16 +0300

    auction partly done

--- src/Auction.js
+++ src/Auction.js
@@ -15,7 +15,17 @@ function Auction() {
                         sagittis odio, non vestibulum felis accumsan sit amet...
                     </p>
                 </div>
-                <div className="bid-box"></div>
+                <div className="bid-box">
+                    <div className="money-auction">$50</div>
+                    <div className="current-bids-auction">Current bids:<span> 5 people</span></div>
+                    <div className="time-left-auction">12 hours remaining</div>
+                    <ul className="buttons-price-auction">
+                        <li className="plus-auction li-bid-auction"></li>
+                        <li className="price-auction li-bid-auction">$100</li>
+                        <li className="minus-auction li-bid-auction"></li>
+                    </ul>
+                    <div className="bid-auction-button">Bid</div>
+                </div>
             </div>
         </div>
 
