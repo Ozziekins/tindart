3e0ce9ab12d032f6f26dc126468ae89079bfe69a
Ozziekins <nenubariozioma@gmail.com>
2021-09-15 21:47:09 +0300

    added bid-box to Auction page

--- src/Auction.js
+++ src/Auction.js
@@ -5,10 +5,20 @@ function Auction() {
     return (
         <div>
             <div className="logo-auction"/>
-            <div className="user"/>
-
-
+            <div className="user-auction"/>
+            <div className="auction">
+                <div className="art-info">
+                    <div className="art-piece"/>
+                    <h3>Art information</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar fringilla porttitor. Maecenas
+                        porttitor ipsum eu ipsum sagittis, sed mollis neque aliquet. Sed vitae molestie ex. Vestibulum dapibus
+                        sagittis odio, non vestibulum felis accumsan sit amet...
+                    </p>
+                </div>
+                <div className="bid-box"></div>
+            </div>
         </div>
+
     );
 }
 
