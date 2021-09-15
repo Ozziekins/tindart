6205c4ade9c33a620a39ff92d7ad16cfbb7aa1a3
annesard <a.ryabkova@innopolis.university>
2021-09-15 17:46:35 +0300

    current state

--- src/Feed.js
+++ src/Feed.js
@@ -5,6 +5,16 @@ function Feed() {
         <div>
             <div className="logo-feed"/>
             <div className="user"/>
+            <div className="post">
+                <div className="post-user-name-time">
+                    <ul>
+                        <li className="post-user"> </li>
+                        <li className="post-username">User Darryl</li>
+
+                    </ul>
+                </div>
+
+            </div>
         </div>
     );
 }
