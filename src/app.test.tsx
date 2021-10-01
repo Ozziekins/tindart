bf9036049580c231695577715fdacd04f1f65cad
Ozziekins <nenubariozioma@gmail.com>
2021-10-01 22:49:29 +0300

    2 working tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -7,6 +7,8 @@ import { cleanup, render } from '@testing-library/react'
 import { PicFeed } from './pages/feed/Feed.styles'
 import { getImage } from './pages/feed/Feed'
 import './components/profile/NavProfile'
+import { shallow } from 'enzyme'
+import Searchbutton from './components/searchbutton/Searchbutton'
 
 afterEach(cleanup)
 
@@ -19,6 +21,22 @@ describe('Get image by ID', () => {
       <PicFeed style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: '450px 357px', objectFit: 'contain' }} />
     )
     expect(getImage(input.image_id)).toEqual(output)
+  })
+})
+
+describe('BaseButton', () => {
+  describe('when user clicks button', () => {
+    it('calls correct function to save the information', () => {
+      const onButtonClickMock = jest.fn()
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const wrapper = shallow(<Searchbutton onButtonClick={onButtonClickMock} />)
+      const buttonElement = wrapper.find('.form-test') // step 1 above
+      buttonElement.simulate('click') // step 2
+
+      expect(onButtonClickMock).toHaveBeenCalledTimes(1) // step 3
+      expect(onButtonClickMock).toHaveBeenCalledWith(true)
+    })
   })
 })
 
